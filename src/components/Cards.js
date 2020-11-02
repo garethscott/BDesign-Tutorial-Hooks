85c984b2753cee6d98005f7b47169d11205caa3b
garethscott <59648675+garethscott@users.noreply.github.com>
2020-11-02 16:31:34 +0000

    further changes to website tutorial

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItems from './CardItems'
+import './Cards.css';
 
 function Cards() {
     return (
@@ -8,7 +9,38 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__item">
-                        <CardItems />
+                        <CardItems
+                            src='images/img-9.jpg'
+                            text='Explore the hidden water fall inside the amazon jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItems
+                            src='images/img-2.jpg'
+                            text='Explore the hidden water fall inside the amazon jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                    </ul>
+                    <ul className="cards__item">
+                        <CardItems
+                            src='images/img-9.jpg'
+                            text='Explore the hidden water fall inside the amazon jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItems
+                            src='images/img-2.jpg'
+                            text='Explore the hidden water fall inside the amazon jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItems
+                            src='images/img-2.jpg'
+                            text='Explore the hidden water fall inside the amazon jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
                     </ul>
                 </div>
             </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Join the Adventure news let
+              </p>
+                <p className="footer-subscription-text">
+                    Join the Adventure news let
+              </p>
+            </section>
+        </div>
+    )
+}
